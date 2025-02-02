cc329ba1663207813d93e0b6e466e40fdbb837e4
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-02-01 22:24:03 -0400

    Fixing input id's

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -21,21 +21,22 @@ export default function FormComponent() {
         <section className="w-full bg-[#77C51E] py-20 px-auto">
             <form action="" className="text-black w-4/5 mx-auto p-10 space-y-5 bg-white rounded-lg">
                 <div className="flex flex-col gap-2">
-                    <label htmlFor="" className="font-bold">Category</label>
-                    <select className=" border border-gray-400 rounded-lg p-1" name="" id="">
+                    <label htmlFor="category" className="font-bold">Category</label>
+                    <select className=" border border-gray-400 rounded-lg p-1" id="category">
                         {
                             categories.map((item) => (
                                 <option key={item.id} value={item.id}>{item.name}</option>
                             ))
                         }
-                    </select>                </div>
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="" className="font-bold">Activity</label>
-                    <input type="text" className=" border border-gray-400 rounded-lg p-1" />
+                    </select>                
                 </div>
                 <div className="flex flex-col gap-2">
-                    <label htmlFor="" className="font-bold">Calories</label>
-                    <input type="text" className=" border border-gray-400 rounded-lg p-1" />
+                    <label htmlFor="activity" className="font-bold">Activity</label>
+                    <input type="text" className=" border border-gray-400 rounded-lg p-1" id="activity" />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="calories" className="font-bold">Calories</label>
+                    <input type="text" className=" border border-gray-400 rounded-lg p-1" id="calories" />
                 </div>
                 <button className="bg-gray-800 text-white text-xl font-semibold w-full p-2">Save</button>
             </form>
