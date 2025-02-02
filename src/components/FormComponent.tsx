eaaa8f2a2d5c01908270136fab17865a5c008afd
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-02-01 23:16:41 -0400

    Adding form validation

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { categories } from "../data/categories";
 
 type activity = {
@@ -21,7 +21,11 @@ export default function FormComponent() {
     const handleChange = (e : React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement> ) => {
         setActivity({
             ...activity,
-            [e.target.id === "activity"? "name" : e.target.id] : e.target.value
+            [e.target.id === "activity"? "name" : e.target.id] :
+                    e.target.id === "category" || e.target.id === "calories"?
+                        Number.parseInt(e.target.value) :
+                        e.target.value
+
         })
     }
 
@@ -33,7 +37,9 @@ export default function FormComponent() {
         ])
     }
 
-    useEffect(() => console.log(activity), [activity])
+   const isValidSubmit = () => {
+    return activity.name.trim() !== '' && activity.calories > 0? true : false
+   } 
 
     return (
         <section className="w-full bg-[#77C51E] py-20 px-auto">
@@ -71,9 +77,15 @@ export default function FormComponent() {
                         id="calories" 
                         value={activity.calories} 
                         onChange={handleChange}
+                        min={0}
                     />
                 </div>
-                <button className="bg-gray-800 text-white text-xl font-semibold w-full p-2">Save</button>
+                <button 
+                    className="bg-gray-800 text-white text-xl font-semibold w-full p-2"
+                    disabled={!isValidSubmit}
+                >
+                    Save
+                </button>
             </form>
         </section>
     )
