6cee4168fc82636fbb04e7395f7de26be9043e19
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-02-06 23:21:25 -0400

    Fixing the filter for category

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,14 +1,19 @@
 import { activity } from "../types"
+import { categories } from "../data/categories"
+import { useMemo } from "react"
 
 export default function ActivityList({activities} : {activities: activity[]}) {
+
+    const categoryName = useMemo(() => (id : number) => categories.map(item => item.id === id? item.name : '' ), [activities])
+
   return (
-    <section className="w-4/5 mx-auto space-y-4 mt-10">
+    <section className="w-4/5 mx-auto space-y-4 mt-10 p-4">
       <h3 className="text-slate-600 text-center text-4xl font-bold">Food and activities</h3>
-      <div className="space-y-4">
+      <div className="space-y-10">
         {
         activities.map(activity => (
-            <div className="bg-white p-6 text-black">
-                <p>{activity.category}</p>
+            <div className="relative bg-white p-6 text-black shadow-lg">
+                <p className={`font-bold text-lg py-2 px-10 -top-5 text-white -left-5 text-center absolute uppercase ${activity.category === 0 ? 'bg-lime-500' : 'bg-orange-500' }`}>{categoryName(activity.category)}</p>
                 <div className="space-y-3">
                     <p className="font-bold text-xl">{activity.name}</p>
                     <h3 className="text-3xl text-lime-500 font-black">{activity.calories} Calories</h3>
