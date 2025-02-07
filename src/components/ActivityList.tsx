31cb0eef2cebd005a86fcab7d1f79133faf55886
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-02-07 14:35:35 -0400

    Fixing objets position

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -15,12 +15,14 @@ export default function ActivityList({activities} : {activities: activity[]}) {
         activities.map(activity => (
             <div className="relative bg-white p-6 text-black shadow-lg">
                 <p className={`font-bold text-lg py-2 px-10 -top-5 text-white -left-5 text-center absolute uppercase ${activity.category === 0 ? 'bg-lime-500' : 'bg-orange-500' }`}>{categoryName(activity.category)}</p>
-                <div className="space-y-3">
-                    <p className="font-bold text-xl">{activity.name}</p>
-                    <h3 className="text-3xl text-lime-500 font-black">{activity.calories} Calories</h3>
-                </div>
-                <div className="flex gap-5 items-center">
-                  <PencilSquareIcon className="size-8 text-gray-800" />
+                <div className="flex justify-between items-center">
+                  <div className="space-y-3 mt-5">
+                      <p className="font-bold text-xl">{activity.name}</p>
+                      <h3 className="text-3xl text-lime-500 font-black">{activity.calories} Calories</h3>
+                  </div>
+                  <div className="flex gap-5 items-center">
+                    <PencilSquareIcon className="size-8 text-gray-800" />
+                  </div>
                 </div>
             </div>
         ))
