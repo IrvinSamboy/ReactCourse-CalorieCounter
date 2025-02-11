7debc82ab03bf6bc4b0b4d864592944dcd5050c9
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-02-11 10:17:46 -0400

    Adding restriction for restart app if the aplication doesnt have activities

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react"
+import { useMemo, useReducer } from "react"
 import FormComponent from "./components/FormComponent"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
 import ActivityList from "./components/ActivityList"
@@ -7,14 +7,17 @@ function App() {
 
   const [state, dispatch] = useReducer(activityReducer, initialState)
   
+  const canRestartApp = useMemo(() => state.activities.length > 0, [state.activities])
+
   return (
     <div className="w-screen text-white">
       <header className="bg-[#599814] p-4">
           <div className="w-4/5 mx-auto flex justify-between items-center">
             <p className="font-bold text-2xl">Calories counter</p>
             <button
-              onClick={() => dispatch({type: 'restart-app'})} 
-              className="rounded-lg bg-gray-900 text-sm font-semibold px-3 py-2">Restart app</button>
+              onClick={() => dispatch({type: 'restart-app'})}
+              disabled={!canRestartApp}
+              className="rounded-lg bg-gray-900 text-sm font-semibold px-3 py-2 disabled:opacity-50">Restart app</button>
           </div>
       </header>
       <FormComponent
