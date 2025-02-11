f44385c94f0974be1f89762edf92f6bd4c898472
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-02-11 11:29:14 -0400

    Finalizing colorie summary component

--- src/components/CalorieSummary.tsx
+++ src/components/CalorieSummary.tsx
@@ -5,9 +5,30 @@ type calorieSummaryProps = {
 }
 
 export default function CalorieSummary({activities} : calorieSummaryProps) {
-  return (
-    <div>
-      
+    
+    const getCalorieTotal = (category : 0 | 1) => {
+        return activities.reduce((total, item) => item.category === category? total + item.calories: total, 0)
+    }
+  
+    return (
+    <div className="bg-gray-900 py-5">
+      <div className="w-4/5 mx-auto space-y-4">
+        <p className="text-3xl text-center font-bold">Calories Summary</p>
+        <div className="flex justify-between">
+            <div className="space-y-2 text-center">
+                <h2 className="text-4xl font-bold">{getCalorieTotal(0)}</h2>
+                <p className="font-medium">Consumed</p>
+            </div>
+            <div className="space-y-2 text-center">
+                <h2 className="text-4xl font-bold">{getCalorieTotal(1)}</h2>
+                <p className="font-medium">Excercise</p>
+            </div>
+            <div className="space-y-2 text-center">
+                <h2 className="text-4xl font-bold">{getCalorieTotal(0) - getCalorieTotal(0)}</h2>
+                <p className="font-medium">Diferences</p>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
